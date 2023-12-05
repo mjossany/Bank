9ed526d2818a1cf86846d59fcbc48facb7ec871e
Jossany Moura <mjossany@gmail.com>
2023-12-05 17:22:17 -0300

    Enhancing documentation of REST APIs using @Schema & example data - Part 1

--- accounts/src/main/java/com/jossadev/accounts/dto/AccountsDto.java
+++ accounts/src/main/java/com/jossadev/accounts/dto/AccountsDto.java
@@ -1,19 +1,34 @@
 package com.jossadev.accounts.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
+@Schema(
+        description = "Accounts",
+        example = "Schema to hold Account information"
+)
 public class AccountsDto {
 
+    @Schema(
+            description = "Accounts"
+    )
     @NotEmpty(message = "AccountNumber can not be a null or empty")
     @Pattern(regexp = "(^$|[0-9]{10})", message = "AccountNumber must be 10 digits")
     private Long accountNumber;
 
+    @Schema(
+            description = "Account type of Eazy Bank account",
+            example = "Savings"
+    )
     @NotEmpty(message = "AccountType can not be a null or empty")
     private String accountType;
 
+    @Schema(
+            description = "BranchAddress can not be a null or empty"
+    )
     @NotEmpty(message = "BranchAddress can not be a null or empty")
     private String branchAddress;
 }
